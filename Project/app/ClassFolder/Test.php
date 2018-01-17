68a71fca1074b1b211d625d517b031be66e56a2d
kedorev <kedorev@gmail.com>
2018-01-17 14:18:36 +0100

    start phpmetric

---
+++ Project/app/ClassFolder/Test.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apprenant
+ * Date: 17/01/18
+ * Time: 12:04
+ */
+
+namespace App\Class
+
---
+++ Project/app/ClassFolder/TestPhpmetric.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: apprenant
+ * Date: 17/01/18
+ * Time: 12:12
+ */
